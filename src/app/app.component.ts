16c2decda3a38ec63abe7f2e2f310c50c6048207
Cheryl Frankenfield and Holly Wilkalis <holly@>
2018-01-22 15:36:46 -0800

    Add content to new and home components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hacker News';
+
+  posts: Post[] = [
+    new Post("Dog rides a motorcycle across country", "https://mondaynote.com/saving-our-children-from-smartphones-22654590ca9d", "mondaynote.com", 1, "Osiris", 5, ["alskjflsajf", "aksjfkjsldfkjalks", "hello"]),
+
+    new Post("What makes a Failure a Disaster?", "http://blog.launchdarkly.com/what-makes-a-failure-a-disaster/", "launchdarkly.com", 3, "Dude", 55, ["This was a great article","you are a fraud","will you marry me?"]),
+
+    new Post("Crowbars: Vending Machines Reward Crows for Cleaning Up Cigarette Butts", "https://weburbanist.com/2018/01/20/crowbars-vending-machines-reward-crows-for-cleaning-up-cigarette-butts/", "weburbanist.com", 3, "misnamed", 3, [])
+  ];
 }
--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 import { Post } from '../post.model';
+import { AppComponent } from '../app.component';
+
 
 @Component({
   selector: 'app-new',
@@ -8,13 +10,8 @@ import { Post } from '../post.model';
 })
 
 export class NewComponent implements OnInit {
-  posts: Post[] = [
-    new Post("Dog rides a motorcycle across country", "https://mondaynote.com/saving-our-children-from-smartphones-22654590ca9d", "mondaynote.com", 1, "Osiris", 5, ["alskjflsajf", "aksjfkjsldfkjalks", "hello"]),
 
-    new Post("What makes a Failure a Disaster?", "http://blog.launchdarkly.com/what-makes-a-failure-a-disaster/", "launchdarkly.com", 3, "Dude", 55, ["This was a great article","you are a fraud","will you marry me?"]),
-
-    new Post("Crowbars: Vending Machines Reward Crows for Cleaning Up Cigarette Butts", "https://weburbanist.com/2018/01/20/crowbars-vending-machines-reward-crows-for-cleaning-up-cigarette-butts/", "weburbanist.com", 3, "misnamed", 3, [])
-  ];
+  @Input() childPosts: Post[];
 
   // webSearch(headline) {
   //   return 'https://www.google.com/search?q=' + this.headline;
@@ -24,6 +21,6 @@ export class NewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
 
+  }
 }
